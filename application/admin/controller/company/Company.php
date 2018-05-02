87a123c471ece5116b1cd025fefd6a66b4aec9bf
znlccy <756311868@qq.com>
2018-05-02 10:59:25 +0800

    修改公司详情界面和保存公司基本信息

--- application/admin/controller/company/Company.php
+++ application/admin/controller/company/Company.php
@@ -5,7 +5,8 @@ namespace app\admin\controller\company;
 use app\common\controller\Backend;
 use app\admin\model\Company as ConfigModel;
 use think\Db;
-use think\Request as request;
+use think\Request;
+use think\Url;
 
 /**
  * 
@@ -105,6 +106,53 @@ class Company extends Backend
      */
     public function trace() {
         $this->redirect('admin/company/tracelog');
+    }
+
+    /**
+     * 保存基本信息
+     */
+    public function saveBasicInfo() {
+        //获取请求的数据
+        $data = Request::instance()->post();
+        if ($data == null) {
+            $this->error('请求数据为空');
+        }
+        $basicInfo = $data['row'];
+        if ($basicInfo == null) {
+            $this->error('输入基本信息为空');
+        }
+        $cellphone = $basicInfo['cellphone'];
+        $introduce = $basicInfo['introduce'];
+        $link = $basicInfo['link'];
+        $manager = $basicInfo['manager'];
+        $password = $basicInfo['password'];
+        $grade = $basicInfo['grade'];
+        $expired = $basicInfo['expired'];
+        $status = $basicInfo['status'];
+
+        $query = Db::table('tb_company')->where('cellphone', $cellphone)->select();
+        if ($query == null) {
+            $this->error('没有该项数据');
+        } else{
+            $result = Db::table('tb_company')->where('cellphone', $cellphone)->update(['introduce' => $introduce, 'link' => $link, 'manager' => $manager, 'password' => md5($password), 'grade' => $grade, 'expired' => $expired, 'status' => $status]);
+            if ($result == 1) {
+                $this->success('保存数据成功');
+            } else {
+                $this->error('保存数据失败');
+            }
+        }
+
+        $url = 'javascript: window.close();';
+        Url::build($url);
+        echo "<script>window.close();</script>";
+        var_dump($basicInfo);
+    }
+
+    /**
+     *
+     */
+    public function getManagerList() {
+
     }
 
 }
